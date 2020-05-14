dc9d7ff6825f5578afed241531ad147cdf60a97b
farissm <farismuhammad2198@gmail.com>
2020-05-14 20:52:57 +0700

    fetch question

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
 
+const fs = require('fs')
 const epxress = require("express");
 const app = epxress();
 const port = process.env.PORT || 3000;
@@ -14,10 +15,15 @@ const io = require("socket.io")(http);
 const cors = require("cors");
 app.use(cors());
 
+let questionData = JSON.parse(fs.readFileSync('./questions.json', 'utf8'))
 // ==================================================
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on('question-data', () => {
+    socket.emit('question-data', questionData)
+  })
 });
 
 // ==================================================
